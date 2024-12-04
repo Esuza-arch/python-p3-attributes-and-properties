a014a9abe1c9e65f4551bf6f396d9d6349f368d5
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-12-04 21:43:28 +0300

    Complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,23 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    APPROVED_BREEDS = ["Labrador", "Pug", "Bulldog", "Beagle", "Chihuahua"]
+
+    def __init__(self, name=None, breed=None):
+        if name is not None:
+            if not isinstance(name, str) or not (1 <= len(name) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+                return
+            self.name = name
+        else:
+            self.name = None
+
+        if breed is not None:
+            if breed not in self.APPROVED_BREEDS:
+                print("Breed must be in list of approved breeds.")
+                return
+            self.breed = breed
+        else:
+            self.breed = None
+
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,23 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    APPROVED_JOBS = ["Sales", "Engineering", "HR", "ITC", "Marketing"]
+
+    def __init__(self, name=None, job=None):
+        if name is not None:
+            if not isinstance(name, str) or not (1 <= len(name) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+                return
+            self.name = name.title()
+        else:
+            self.name = None  
+
+        if job is not None:
+            if job not in self.APPROVED_JOBS:
+                print("Job must be in list of approved jobs.")
+                return
+            self.job = job
+        else:
+            self.job = None
+
+
